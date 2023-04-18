5d53300425954eac130fa8bd7a972d340e4b9c1e
Naturvard <91907848+Naturvard@users.noreply.github.com>
2023-04-18 11:39:33 +0530

    Add files via upload

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -6,7 +6,7 @@ int RemoveDuplicates(int arr[], int n)
     int res = 1;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != arr[res])
+        if (arr[i] != arr[res-1])
         {
             arr[res] = arr[i];
             res++;
